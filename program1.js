5da4e919fb747f39875f071229856cfcefdfd2e2
Abhinav9644 <155716274+Abhinav9644@users.noreply.github.com>
2024-01-05 13:27:29 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,7 +2,40 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function(s) {function isValidParentheses(str) { 
+	const stack = []; 
+	const pairs = { 
+		"(": ")", 
+		"[": "]", 
+		"{": "}", 
+	}; 
+
+	for (let char of str) { 
+		if (pairs[char]) { 
+			stack.push(char); 
+		} else if ( 
+			char === ")" || 
+			char === "]" || 
+			char === "}"
+		) { 
+			if ( 
+				pairs[stack.pop()] !== 
+				char 
+			) { 
+				return false; 
+			} 
+		} 
+	} 
+
+	return stack.length === 0; 
+} 
+
+const inputString = "({()})"; 
+console.log( 
+	`Is it a valid Paranthesis ? : 
+${isValidParentheses(inputString)}` 
+); 
+
     
 };
 
